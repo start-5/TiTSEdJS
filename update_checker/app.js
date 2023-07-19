000dddc9153930fac2e5f095cb118335f9f93a55
start-5 <titsedjs@outlook.com>
2023-07-19 19:14:11 -0400

    start update checker

---
+++ update_checker/app.js
@@ -0,0 +1,13 @@
+﻿const Parser = require('rss-parser');
+
+const parser = new Parser();
+
+const feedUrl = 'https://www.fenoxo.com/feed/';
+
+(async () => {
+
+    //const feed = await parser.parseURL(feedUrl);
+
+    process.exitCode = 199;
+
+})();
